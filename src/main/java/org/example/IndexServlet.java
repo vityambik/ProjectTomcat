75c4dfa5f3be76c635f53e7be7598322f6c185e9
Vityambik <vityambik13@gmail.com>
2024-02-06 14:27:22 +0600

    rename project and add servlets

--- src/main/java/org/example/IndexServlet.java
+++ src/main/java/org/example/IndexServlet.java
@@ -1,2 +1,18 @@
-package org.example;public class IndexServlet {
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/hello").forward(req, resp);
+//        resp.sendRedirect(req.getContextPath() + "/hello");
+    }
 }
--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + "<br>");
+        } else {
+            printWriter.write("Hello, " + username + "<br>");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
+        printWriter.close();
     }
 }
